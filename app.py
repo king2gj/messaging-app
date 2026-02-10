3cad711b44a246970a447e438acdfbb1c959ac65
sansvoice <cjaymeme@gmail.com>
2026-02-09 19:23:30 -0500

    test post page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/test')
+def testpost():
+    return render_template("testpost.html")
 
 if __name__ == '__main__':
     app.run()
