7356602fffc3ba21254eefa420e86310fffbda02
Nateb2022 <natebarnhart022@gmail.com>
2026-02-17 10:01:55 -0500

    Starting on routing

--- page.py
+++ page.py
@@ -1,6 +1,76 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
+
+def login():
+    return "Login successful."
+
 @app.route("/")
-def hello(name=None):
+def index(name=None):
     return render_template('base_post.html', person=name)
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+
+
+@app.route("/signup")
+def signup():
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        return login()
+        #database access logic
+    else:
+        return render_template("signin.html")
+        
+
+@app.route("/signout")
+def signout():
+
+@app.route("/account")
+def account():
+
+@app.route("/update_account")
+def update_account():
+
+@app.route("/rep_user")
+def rep_user():
+
+@app.route("/del_user")
+def del_user():
+
+@app.route("/view_post")
+def view_post():  
+
+@app.route("/post") 
+def post():
+
+@app.route("/del_post")
+def del_post():
+
+@app.route("/rep_post")
+def rep_post():
+
+@app.route("/edit_post")
+def edit_post():
+
+@app.route("/lock_post")
+def lock_post():
+
+@app.route("/view_thread")
+def view_thread():
+
+@app.route("/thread")
+def thread():
+
+@app.route("/del_thread")
+def del_thread():
+
+@app.route("/rep_thread")
+def rep_thread():
+
+@app.route("/edit_thread")
+def edit_thread():
+
+@app.route("/lock_thread")
+def lock_thread():
