a69c1bf37a3142b59234bd2741724d52368a9b61
VL5H <vishsocial5@gmail.com>
2026-03-26 12:01:09 -0400

    Modified constructors for both user classes to follow the same all-args format. Changed class names to StandardUser and AdminUser to follow naming conventions. Moved placeholder admin functions from AdminUser class to the AdminConsole class in admin_console.py.

--- users.py
+++ users.py
@@ -1,23 +1,49 @@
 import datetime
 import uuid
 
-class standard_user:
-    def __init__(self, email, password):
-        self.user_ID = uuid.uuid4()
-        self.name = email
+class StandardUser:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        
+        if not hasattr(self, 'user_ID'):
+            self.user_ID = uuid.uuid4()
+
+        if not hasattr(self, 'username'):
+            self.username = None
+
         self.type = "standard_user"
         self.is_admin = False
-        self.date_joined = datetime.datetime.now()
-        self.last_online = None
-        self.online = False
-        self.posts = []
-        self.post_count = 0
-        self.bio = None
-        self.profile_picture = None # change to a path to a file later
-        # below 2 attributes will be changed to accessing an authentication system. probably a json containing hashed values for now.
-        self.username = email
-        self.password = password
-        self.auth_success = False
+
+        if not hasattr(self, 'date_joined'):
+            self.date_joined = datetime.datetime.now()
+
+        if not hasattr(self, 'last_online'):
+            self.last_online = datetime.datetime.now()
+
+        if not hasattr(self, 'online'):
+            self.online = True
+
+        if not hasattr(self, 'posts'):
+            self.posts = []
+
+        if not hasattr(self, 'post_count'):
+            self.post_count = 0
+
+        if not hasattr(self, 'bio'):
+            self.bio = None
+
+        if not hasattr(self, 'profile_picture'):
+            self.profile_picture = None
+        
+        if not hasattr(self, "email"):
+            self.email = None
+        
+        if not hasattr(self, "password"):
+            self.password = None
+
+        if not hasattr(self, 'auth_success'):
+            self.auth_success = False
 
 
     def login(self, authenticator):
@@ -55,34 +81,82 @@ class standard_user:
         self.profile_picture = profile_picture
         return f"Profile updated."
 
-class admin_user(standard_user):
-    def __init__(self, name, email, password):
-        super().__init__(name, email, password)
-        self.user_ID = uuid.uuid4()
+class AdminUser:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        
+        if not hasattr(self, 'user_ID'):
+            self.user_ID = uuid.uuid4()
+
+        if not hasattr(self, 'username'):
+            self.username = None
+
         self.type = "admin_user"
         self.is_admin = True
-        self.posts = []
-        self.post_count = 0
-        self.bio = None
-        self.profile_picture = None # change to a path to a file later
-        self.email = email
-        self.password = password
-        self.auth_success = False
+
+        if not hasattr(self, 'date_joined'):
+            self.date_joined = datetime.datetime.now()
+
+        if not hasattr(self, 'last_online'):
+            self.last_online = datetime.datetime.now()
+
+        if not hasattr(self, 'online'):
+            self.online = True
+
+        if not hasattr(self, 'posts'):
+            self.posts = []
+
+        if not hasattr(self, 'post_count'):
+            self.post_count = 0
+
+        if not hasattr(self, 'bio'):
+            self.bio = None
+
+        if not hasattr(self, 'profile_picture'):
+            self.profile_picture = None
+        
+        if not hasattr(self, "email"):
+            self.email = None
+        
+        if not hasattr(self, "password"):
+            self.password = None
+
+        if not hasattr(self, 'auth_success'):
+            self.auth_success = False
     
+    
+    def login(self, authenticator):
+        if authenticator and self.online == False:
+            self.online = True
+            self.last_online = datetime.datetime.now()
+            self.auth_success = True
+            return f"{self.name} has logged in."
+        elif authenticator and self.online == True:
+            self.auth_success = False
+            return f"{self.name} is already logged in."
+        else:
+            self.auth_success = False
+            return "Invalid email or password."
 
-    def delete_user(self, user):
-        pass
-        # this will be changed to accessing a deletion system.
+    def logout(self):
+        self.online = False
+        self.last_online = datetime.datetime.now()
+        return f"{self.name} has logged out."
 
-    def ban_user(self, user):
-        pass
-        # this will be changed to accessing a banning system. 
+    def get_profile(self):
+        return f"Name: {self.name}\nType: {self.type}\nDate Joined: {self.date_joined}\nOnline: {self.online}\nLast Online: {self.last_online}\nBio: {self.bio}\nPosts: {self.post_count}"
 
-    def unban_user(self, user):
+    def report_user(self):
         pass
-        # this will be changed to accessing a banning system. 
+        # this will be changed to accessing a reporting system. probably a json containing reported users for now.
 
-    def delete_post(self, post):
+    def delete_account(self):
         pass
-        # this will be changed to accessing a deletion system. 
- 
+        # this will be changed to accessing a deletion system. probably a json containing deleted users for now.
+
+    def edit_profile(self, name = None, bio = None, profile_picture = None):
+        self.name = name
+        self.bio = bio
+        self.profile_picture = profile_picture
+        return f"Profile updated."
